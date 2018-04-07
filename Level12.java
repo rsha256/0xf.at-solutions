86fd692d45b4abb25625cc713e6e96a808fd988d
Rahul Shah <rahuldshah3@gmail.com>
2018-04-07 00:08:27 -0400

    Solution 12 - Summation

--- Level12.java
+++ Level12.java
@@ -1,25 +1,17 @@
 import java.util.Scanner;
-
 public class Level12 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter first bound: ");
+        System.out.println("Enter first #: ");
         int a = input.nextInt();
-
-        System.out.println("Enter second bound: ");
+        System.out.println("Enter second #: ");
         int b = input.nextInt();
-
-        if (a == 1) {
-            // This method is better but only works if you are finding a summation with a starting term of 1
-            System.out.println("\n" + b * (b + 1) / 2);
-        } else {
-            // The second method works with any bounds
+        if (a == 1) // This method is better but only works if you are finding a summation with a starting term of 1
+            System.out.println("\nSum: " + b * (b + 1) / 2);
+        else // This second method works with any bounds
             sum(a, b);
-        }
     }
-    private static int sumMonotonic(int a, int b)
-    {
+    private static int sumMonotonic(int a, int b) {
         int result = 0;
         while (a <=b) {
             result+=a;
@@ -27,14 +19,12 @@ public class Level12 {
         }
         return result;
     }
-
-    private static void sum(int a, int b)
-    {
+    private static void sum(int a, int b) {
         int result;
         if (a<=b)
             result = sumMonotonic(a,b);
         else
             result = sumMonotonic(b,a);
-        System.out.println("The sum of all numbers is "+result);
+        System.out.println("Sum: "+result);
     }
 }
