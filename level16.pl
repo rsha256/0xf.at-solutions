51bd288935065eaf668a9f1d7826c27dfab37efc
Rahul Shah <rahuldshah3@gmail.com>
2018-04-07 00:23:25 -0400

    Update level16.pl

--- level16.pl
+++ level16.pl
@@ -1,4 +1,5 @@
-# run the following command in a unix terminal
+# run the following command in a terminal
 # where perl is installed
+# (perl 6)
 
 # perl -MMIME::Base64 -e 'print "\n",decode_base64("YzBhMmMxYjc0YzJjMTM3ODhmZTM1NTY3Zg=="),"\n";'
